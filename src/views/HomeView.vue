0f2d1ab0e4a30f6721fb10d1406b98cecb3d1b95
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-06-11 23:40:12 +0200

    Implemented map without view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { MapboxMap, MapboxNavigationControl } from 'vue-mapbox-ts';
+import { Map as MapboxMapObject } from 'mapbox-gl';
+
+const accessToken =
+  'pk.eyJ1Ijoiamlhbmd4aXV6aGVuIiwiYSI6ImNseGFzbWk3MTA3NXYybXMzNHN5OTVpbXAifQ.Ou5LyHrssSEh3dlhsGjcHg';
+const mapStyle = 'mapbox://styles/mapbox/streets-v11';
+const mapCenter = [14.51446, 48.368721];
+const mapZoom = 15;
+
+console.log('Loading Map...');
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <mapbox-map
+    :accessToken="accessToken"
+    :mapStyle="mapStyle"
+    :draggable="true"
+    :center="mapCenter"
+    :zoom="mapZoom"
+    class="basemap"
+  >
+    <!-- <mapbox-marker :lngLat="[14.51446, 48.368721]"> </mapbox-marker> -->
+  </mapbox-map>
 </template>
+
+<style></style>
