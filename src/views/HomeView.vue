1e0287af7436ad9663f425ddb5b7a0920a0b0d54
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-06-12 23:46:22 +0200

    Made new web app with purchase button and tests

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,78 +1,44 @@
 <template>
-  <button v-on:click="toggleStatus">
-    <p v-if="status">Hide missions</p>
-    <p v-else>Show missions</p>
+  <button v-on:click="purchase">
+    <p v-if="purchased">Undo</p>
+    <p v-else>Purchase</p>
   </button>
-
-  <table v-show="status">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Location</th>
-        <th>Coordinates</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="mission in missions" :key="mission.id">
-        <td>{{ mission.id }}</td>
-        <td>{{ mission.location }}</td>
-        <td>{{ mission.coor }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <p>{{ message }}</p>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed, onMounted } from 'vue';
 
-export interface Mission {
-  id: number;
-  location: string;
-  coor: string;
-}
+export const purchase = () => {
+  const businessHours = [9, 18];
+  const currentHour = new Date().getHours();
+  const [open, close] = businessHours;
 
-export const fetchMissions = async (): Promise<Mission[]> => {
-  const response = await fetch('http://localhost:3000/missions');
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  const data: Mission[] = await response.json();
-  return data;
+  if (currentHour > open && currentHour < close) return { message: 'Success' };
+
+  return { message: 'Error' };
 };
 
 export default defineComponent({
   name: 'HomeView',
   setup() {
-    // Show or hide BUTTON
-    const status = ref<boolean>(true);
-
+    const purchased = ref<boolean>(false);
+    const message = ref<string>('Nothing happened yet');
     const toggleStatus = () => {
-      status.value = !status.value;
-    };
-
-    const missions = ref<Mission[]>([]);
-
-    const loadMissions = async () => {
-      try {
-        missions.value = await fetchMissions();
-      } catch (error) {
-        console.error('There was a problem with the fetch operation:', error);
+      if (!purchased.value) {
+        const purchaseData = purchase();
+        message.value = 'Purchase: ' + purchaseData.message;
+      } else {
+        console.log('selling...');
+        message.value = '';
       }
+      purchased.value = !purchased.value;
     };
-
-    onMounted(() => {
-      loadMissions();
-    });
-
-    const statusMessage = computed(() => {
-      return status.value ? 'Status is true' : 'Status is false';
-    });
 
     return {
-      status,
-      toggleStatus,
-      missions,
-      statusMessage,
+      purchased,
+      message,
+      purchase: toggleStatus,
     };
   },
 });
