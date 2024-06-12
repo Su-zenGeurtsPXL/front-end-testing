5e8b1abd074921046dfd7c088cb71aefd5859687
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-06-12 21:54:58 +0200

    Added working examples

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-export const fetchMenu = async (): Promise<any> => {
-    try {
-        const response = await fetch('http://localhost:3000/menu');
-        const menuData = await response.json();
-        return menuData;
-    } catch (error) {
-        throw new Error('Failed to fetch menu items');
-    }
-};
