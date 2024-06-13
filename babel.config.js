1ec14eacc63b96f5c57c84b632299ea9489788bb
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-06-13 13:21:31 +0200

    Attempt with babel but errors

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-    presets: ['@babel/preset-env'],
-  };
-  
+  presets: [
+    '@babel/preset-env', // Transforms ES6+ code to ES5
+    '@babel/preset-typescript', // Adds TypeScript support
+  ],
+  plugins: [
+    '@babel/plugin-transform-runtime', // Adds support for async/await and other features
+    'babel-plugin-transform-vue-jsx', // Adds support for Vue JSX syntax
+  ],
+};
